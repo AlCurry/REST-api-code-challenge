50c54b926e1849c25b051ddefb2f8c9481026c10
Al Curry <alcurry98@gmail.com>
2020-08-26 10:48:21 +0200

    latest improvements

---
+++ src/main/java/com/example/demo/Properties.java
@@ -0,0 +1,25 @@
+/*
+
+   Coding Challenge - develop single endpoint RESTful API with Java 11, Spring Boot, and Docker
+   Al Curry      August 20, 2020
+
+   Geojon objects from this repo were used as a guide.
+   https://github.com/ngageoint/simple-features-geojson-java/tree/master/src/main/java/mil/nga/sf/geojson
+
+*/
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+
+public class Properties {
+
+
+    // Getter Methods
+
+
+    // Setter Methods
+
+
+}
