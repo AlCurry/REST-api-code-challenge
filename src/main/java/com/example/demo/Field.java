9c0b83d0ed8f68359b09f67fc1dc50b25a4d2c19
Al Curry <alcurry98@gmail.com>
2020-08-26 10:46:34 +0200

    latest improvements

--- src/main/java/com/example/demo/Field.java
+++ src/main/java/com/example/demo/Field.java
@@ -1,35 +1,44 @@
+/*
+
+   Coding Challenge - develop single endpoint RESTful API with Java 11, Spring Boot, and Docker
+   Al Curry      August 20, 2020
+
+   Geojon objects from this repo were used as a guide.
+   https://github.com/ngageoint/simple-features-geojson-java/tree/master/src/main/java/mil/nga/sf/geojson
+
+*/
+
 package com.example.demo;
 
-
 import com.fasterxml.jackson.annotation.JsonTypeId;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 
 public class Field {
+    private String name;
+    Geo_json Geo_jsonObject;
 
-        private String id;
-        private String name;
-        //private float price;
 
-        public Field() {
-        }
+    // Getter Methods
 
-        public Field(String id, String name) {
-            this.id = id;
-            this.name = name;
-            //this.price = price;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public Field(String id) {
-            this.id = id;
-        }
-        public String getId() {
-           return id;
-        }
-        public String getName() {
-            return name;
-        }
-        /*public float getPrice() {
-            return price;
-        }*/
+    public Geo_json getGeo_json() {
+        return Geo_jsonObject;
+    }
 
-        // other getters and setters...
+    // Setter Methods
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGeo_json(Geo_json geo_jsonObject) {
+        this.Geo_jsonObject = geo_jsonObject;
+    }
 }
